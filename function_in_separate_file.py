d487e27339ec5864ece0e9850ac723b92fb5a5a0
larrylam16 <55814943+larrylam16@users.noreply.github.com>
2021-10-12 14:31:59 +1100

    Revert "Added Date/Time to logging"

--- function_in_separate_file.py
+++ function_in_separate_file.py
@@ -1,11 +1,2 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-
-import time
-
-
 def Add_file_to_be_called():
-  print(time.strftime("%H:%M:%S") + " this is a fuction from a seperate file")
+  print("this is a fuction from a seperate file")
