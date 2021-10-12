4c0f1d323088addc47cd5d69e5473104167e1ebe
larrylam16 <55814943+larrylam16@users.noreply.github.com>
2021-10-12 14:47:36 +1100

    Added Reading CSV file for login

--- function_in_separate_file.py
+++ function_in_separate_file.py
@@ -7,3 +7,14 @@ import time
 
 def Add_file_to_be_called():
   print(time.strftime("%H:%M:%S") + " this is a fuction from a seperate file")
+
+  
+def Login():
+  credentials = open(r'C:\GitHub\login\credential.csv')
+  csv_credentials = csv.reader(credentials)
+
+  for LoginCredential in csv_credentials:
+		print(LoginCredential[0] + ' , ' + LoginCredential[1] + ' , ' + LoginCredential[2])
+
+  
+  credentials.close()
